2c1c974dd81c93a9a82942387f37441e8971fcc7
Willian vinicius <vini-np7@hotmail.com>
2021-08-18 01:04:36 -0300

    cliação da classe de tratamentos de erro

--- src/share/http/server.ts
+++ src/share/http/server.ts
@@ -1,9 +1,23 @@
-import express from "express";
+import "reflect-metadata";
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import routes from "../http/routes/index";
+import appError from "../errors/appError";
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof appError) {
+    return res.status(error.statusCode).json({
+      status: "error",
+      msg: error.message,
+    });
+  }
+  return res.status(500).json({
+    status: "error",
+    msg: "Internal server error",
+  });
+});
 app.listen(3000);
